de194b0accaea6b6111158eacc78130f71b6ca34
Rayane ZENIA <rayane.zenia@etu.u-cergy.fr>
2026-01-22 22:24:38 +0100

    ajout d'un print

--- test.java
+++ test.java
@@ -1,4 +1,6 @@
 public static void main(String[] args) {
     System.out.println("Hello World test maria ");
     System.out.println("Hello World test sarah ");
+    System.out.println("Helloooo from Rayane ");
+
 }
